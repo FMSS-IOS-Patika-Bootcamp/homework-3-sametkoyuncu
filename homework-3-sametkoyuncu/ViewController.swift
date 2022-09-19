85a13bbadae563fc1c52835ce202ac341fa59567
Samet Koyuncu <sametkoyuncu@live.com>
2022-09-19 11:58:11 +0300

    Create a new project

---
+++ homework-3-sametkoyuncu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework-3-sametkoyuncu
+//
+//  Created by Samet Koyuncu on 19.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
